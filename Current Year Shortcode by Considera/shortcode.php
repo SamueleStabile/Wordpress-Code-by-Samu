c6b69f51d2b3fde56e11fa5bd4d0c2b6e4bd078b
Samuele Stabile <samuelestabilebusiness@gmail.com>
2023-07-28 18:27:24 +0200

    Update shortcode.php

--- Current Year Shortcode by Considera/shortcode.php
+++ Current Year Shortcode by Considera/shortcode.php
@@ -5,7 +5,7 @@
 * Description: Makes available a shortcode [year] that can be used to display the current year - "[year] is the shortcode to use"
 * Version: 0.1
 * Author: Considera Team
-* Author URI: https://considera.it/
+* Author URI: https://github.com/SamueleStabile/Wordpress-Code-by-Samu/tree/main/Current%20Year%20Shortcode%20by%20Considera
 **/
 
 function auto_date(){
