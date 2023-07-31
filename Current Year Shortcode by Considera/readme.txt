98ceafb77eaa764e18d08c8c37d8938c2a9ec607
Samuele Stabile <samuelestabilebusiness@gmail.com>
2023-07-31 11:30:39 +0200

    Wordpress review

---
+++ Current Year Shortcode by Considera/readme.txt
@@ -0,0 +1,10 @@
+=== Current Year Shortcode by Considera ===
+Contributors: samuraider 
+Tags: year, shortcode, considera
+Requires at least: 4.7
+Tested up to: 6.2
+Stable tag: 0.1
+Requires PHP: 7.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Makes available a shortcode [year] that can be used to display the current year - "[year] is the shortcode available"
--- Current Year Shortcode by Considera/shortcode.php
+++ Current Year Shortcode by Considera/shortcode.php
@@ -2,7 +2,7 @@
 /**
 * Plugin Name: Considera Year Shortcode
 * Plugin URI: https://considera.it/
-* Description: Makes available a shortcode [year] that can be used to display the current year - "[year] is the shortcode to use"
+* Description: Makes available a shortcode [year] that can be used to display the current year - "[year] is the shortcode available"
 * Version: 0.1
 * Author: Considera Team
 * Author URI: https://github.com/SamueleStabile/Wordpress-Code-by-Samu/tree/main/Current%20Year%20Shortcode%20by%20Considera
